d3764422c9c4fddebea1c0050f7cd60a083f7f99
Daniel Wootton <225040828+woottondaniel@users.noreply.github.com>
2025-09-23 17:08:39 +0100

    more exercises on conditionals and data types

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+// following https://www.theodinproject.com/lessons/foundations-data-types-and-conditionals
+
+// exercises from https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/Scripting/Strings
+
 const string = "The revolution will not be televised.";
 console.log(string);
 
@@ -56,6 +60,39 @@ console.log(newLine)
 const newLine2 = "One day you finally\nknew what you\nhad to do,\nand began, "
 console.log(newLine2)
 
-
-const bigmouth = 'I\'ve got no right to take my place...'#
+const bigmouth = 'I\'ve got no right to take my place...';
 console.log(bigmouth)
+
+// now doing exercises from https://www.w3schools.com/js/js_string_methods.asp
+
+let text = "Hello world!";
+let char = text.charAt(0);
+console.log(char);
+
+let code = text.codePointAt(0);
+console.log(code)
+
+//codepointat is the unique code of the character and H is code 72 so you get 72
+
+const name4 = "W3Schools";
+let letter = name4[2]; // or name.at(2) for same result
+
+console.log(letter)
+
+let text2 = "hola".concat(" ", "mi amigo")
+console.log(text2)
+
+let text5 = "Apple, Banana, Kiwi";
+let part = text5.slice(7, 13);
+console.log(part)
+
+let text6 = text5.toUpperCase()
+console.log(text6)
+
+let text7 = name4.repeat(2)
+console.log(text7)
+
+let text8 = text5.replace("Banana", "BonBonBonBons")
+console.log(text8)
+
+// following this on conditionals https://www.w3schools.com/js/js_if_else.asp
